70012858c9a8abdb7fc8c40088d95ed6c4fe445a
Lộc Ngô <56879656+LocNgo712@users.noreply.github.com>
2021-01-16 02:09:25 +0700

    Add Again App

---
+++ QLLuatApp/QLLuatApp/QLLuatApp/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.IO;
+using QLLuatApp.Model;
+
+namespace QLLuatApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void SaveLaw(object sender, EventArgs e)
+        {
+            Luat law = (Luat)BindingContext;
+            if (string.IsNullOrWhiteSpace(law.FileName))
+            {
+                law.FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), $"{Path.GetRandomFileName()}.laws.txt");
+            }
+
+            File.WriteAllText(law.FileName, law.LawName) ;
+            File.WriteAllText(law.FileName, law.Text);
+            File.WriteAllText(law.FileName, law.Year);
+
+            await Navigation.PopAsync();
+        }
+
+        async void DeleteLaw(object sender, EventArgs e)
+        {
+            Luat law = (Luat)BindingContext;
+            if (File.Exists(law.FileName))
+            {
+                File.Delete(law.FileName);
+            }
+            await Navigation.PopAsync();
+        }
+    }
+}
